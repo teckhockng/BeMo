0054414fe2b0e5cd24b46e6106ccc51fc2838617
Teck Hock Ng <200349362@student.georgianc.on.ca>
2020-04-29 19:21:23 -0400

    First commit

--- app.js
+++ app.js
@@ -68,6 +68,14 @@ app.use('/contact', contactRouter);
 app.use('/login', sessionsRouter);
 app.use('/logout', logoutRouter);
 
-app.listen(3000);
-console.log('Server running at http://localhost:3000/');
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 module.exports = app;
